47805e7de16e8bc787d63e141a7adcda3f48d629
GrassFedTuna <cst.macphail@gmail.com>
2021-02-12 15:23:57 -0500

    Created script, outputted data, created markdown

--- Download.R
+++ Download.R
@@ -5,5 +5,19 @@ Bburg <- entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") #Retriev
 head(Bburg) #Taking a look at the object
 
 #Creating a new object with sequences seperated
-split <- 
-Sequences <- strsplit(Bburg, )
+Sequences <- strsplit(x = Bburg, "\n\n")
+print(Sequences)
+
+Sequences <- unlist(Sequences) #converting from list to data frame
+
+#removing sequences from header
+header <- gsub("(^>.*sequence)\\n[ATCG].*", "\\1", Sequences)
+
+seq <- gsub("^>.*sequence\\n([ATCG].*)", "\\1", Sequences)
+
+Sequences <- data.frame(Name = header, Sequence = seq)
+
+Sequences$Sequence <- gsub("\n", "", Sequences$Sequence) #Removing newline characters
+head(Sequences$Sequence)
+
+write.csv(Sequences, file = "./Sequences.csv") #Exporting data as .csv file
